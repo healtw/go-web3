dff7d58fb2373cdb034791c49ad634faf46049ec
Carel.cheng <CarelCheng@paxsz.com>
2024-02-01 10:59:25 +0800

    Add source code change records

--- apps/weth/weth.go
+++ apps/weth/weth.go
@@ -1,3 +1,13 @@
+/*
+ * ===========================================================================================
+ * Revision History:
+ * Date                         Author                        Action
+ * 2022/07/08                   Carel.Cheng                   Create
+ *Revised content:
+ *1.Change the path of the imported package
+ * ===========================================================================================
+ */
+
 package weth
 
 import (
